033f94453f2326837f7d6e116b8031212235323a
michaelheinhold <86388353+michaelheinhold@users.noreply.github.com>
2025-09-05 19:13:40 +0000

    Week 2 video

---
+++ src/ex0203.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double x, y;
+    char c;
+
+    do {
+        cout << "Enter operation (+, -, /) or q to terminate: ";
+        cin >> c;
+        if(c =='q') break;
+
+        cout << "Enter x and y (separate by a space): ";
+        cin >> x >> y;
+
+        switch(c){
+            case '+': cout << x+y << endl; break;
+            case '-': cout << x-y << endl; break;
+            case '*': cout << x*y << endl; break;
+            case '/': cout << x/y << endl; break;
+        }
+
+    }while(c != 'q');
+    return 0;
+}
